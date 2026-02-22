5eda0d1876d4df9cbcc7bbff681b988f2157d829
dasithdamsara01-cmd <dasithdamsara01@gmail.com>
2026-02-22 15:37:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 I'm HUNTER BOT.I'm Alive Now😍*",
-BOT_OWNER: '',  // Replace with the owner's phone number
+BOT_OWNER: '94764059948',  // Replace with the owner's phone number
 
 
 
