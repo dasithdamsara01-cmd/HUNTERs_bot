66e843122494cb0f2213d8a96c2e7335ba7ae488
dasithdamsara01-cmd <dasithdamsara01@gmail.com>
2026-02-22 16:53:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/dasithdamsara01-cmd/HUNTERs_bot/blob/main/images/ChatGPT%20Image%20Feb%2022%2C%202026%2C%2004_30_33%20PM.png",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 I'm HUNTER BOT.I'm Alive Now😍*",
 BOT_OWNER: '94764059948',  // Replace with the owner's phone number
 
